683e6aba547fb632fce1f521d0093326c3858133
Bo Zhang <bozhang@nao.cas.cn>
2016-01-01 19:08:50 +0800

    prepare to distutils

---
+++ core.py
@@ -0,0 +1,4 @@
+__version__ = '0.0.1'
+
+def core_print():
+    print 'bopy is coming!'
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'bopy', #*
+    version = '0.0.1',
+    author = 'Bo Zhang',
+    author_email = 'bozhang@nao.cas.cn',
+    py_modules = 'core' # names of all modules in this package
+    description = 'bopy is coming!'
+    )
+
