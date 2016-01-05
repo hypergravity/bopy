1173fb4d9fc0ed055ad5057ae0fa494a77db10cc
Bo Zhang <bozhang@nao.cas.cn>
2016-01-05 21:26:40 +0800

    modified mcmctools.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name = 'bopy', #*
-    version = '0.1', #*
+    version = '0.1.1', #*
     author = 'Bo Zhang', #*
     author_email = 'bozhang@nao.cas.cn', #*
 #    py_modules = ['bopy','spec','core'],
     description = 'bopy is coming!', # short description
     license = 'New BSD',
-    install_requires = ['numpy>=1.7','scipy','matplotlib','nose'],
+#    install_requires = ['numpy>=1.7','scipy','matplotlib','nose'],
     url = 'http://github.com/hypergravity/bopy',
     classifiers = [
         "Development Status :: 6 - Mature",
@@ -20,9 +20,9 @@ setup(
         "Topic :: Scientific/Engineering :: Astronomy",
         "Topic :: Scientific/Engineering :: Physics"],
     package_dir = {'bopy/': ''},
-    packages = ['bopy','bopy/spec','bopy/core'],
+    packages = ['bopy','bopy/spec','bopy/core','bopy/mcmctools'],
     package_data = {'bopy/data':[''],
-                    "": ["README.rst","README.pdf","README.dev","LICENSE","AUTHORS.rst","HISTORY.txt"]},
+                    "": ["LICENSE"]},
     include_package_data = True
     )
 
