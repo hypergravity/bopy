c7934570336f5f7c8e7094dbf8c7f6ad40665a00
Bo Zhang (M2800) <bozhang@nao.cas.cn>
2016-02-13 20:19:41 +0800

    add radial_velocity.py

---
+++ bopy/spec/radial_velocity.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+
+Author
+------
+Bo Zhang
+
+Email
+-----
+bozhang@nao.cas.cn
+
+Created on
+----------
+- Sat Feb  3 19:11:11 2016    read_spectrum
+
+Modifications
+-------------
+- Sat Feb  3 19:11:11 2016    measure_line_index
+
+Aims
+----
+- measure radial velocity / redshift from spectra
+
+"""
