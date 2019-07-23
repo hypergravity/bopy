40c761e992fb0b32c63459e27eb7faa6e1dedfc5
Bo Zhang <bozhang@nao.cas.cn>
2019-07-23 20:29:24 +0800

    updated setup

--- setup.py
+++ setup.py
@@ -1,37 +1,32 @@
-from distutils.core import setup
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-if __name__ == '__main__':
-    setup(
-        name='bopy',
-        version='0.3.0',
-        author='Bo Zhang',
-        author_email='bozhang@nao.cas.cn',
-        # py_modules=['bopy','spec','core'],
-        description='Bo Zhang (@NAOC)''s python package.',  # short description
-        license='New BSD',
-        # install_requires=['numpy>=1.7','scipy','matplotlib','nose'],
-        url='http://github.com/hypergravity/bopy',
-        classifiers=[
-            "Development Status :: 6 - Mature",
-            "Intended Audience :: Science/Research",
-            "License :: OSI Approved :: BSD License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 2.7",
-            "Topic :: Scientific/Engineering :: Astronomy",
-            "Topic :: Scientific/Engineering :: Physics"],
-        package_dir={'bopy/': ''},
-        packages=['bopy',
-                  'bopy/spec',
-                  'bopy/core',
-                  'bopy/mcmctools',
-                  'bopy/obstools',
-                  'bopy/obstools/Xinglong216HRS',
-                  'bopy/helpers',
-                  'bopy/helpers/ezpadova'
-                  ],
-        package_data={'bopy/data': [''],
-                      "":          ["LICENSE"]},
-        include_package_data=True,
-        requires=['numpy', 'scipy', 'matplotlib', 'astropy', 'lmfit']
-    )
+setuptools.setup(
+    name='bopy',
+    version='0.4.0',
+    author='Bo Zhang',
+    author_email='bozhang@nao.cas.cn',
+    description='Bo Zhang (@NAOC)''s python package.', # short description
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/hypergravity/bopy',
+    packages=setuptools.find_packages(),
+    #packages=['song', 'twodspec'],
+    license='New BSD',
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "Intended Audience :: Science/Research",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python :: 3.7",
+                 "Topic :: Scientific/Engineering :: Physics",
+                 "Topic :: Scientific/Engineering :: Astronomy"],
+    # package_dir={'song': 'song',
+    #              'twodspec': 'twodspec'}, commented because it's wrong
+    package_data={"bopy": ['bopy/data/test_spectra/*/*.fits', ],
+                  "": ["LICENSE"]
+                  },
+    # include_package_data=False, # commented to include data!
+    requires=['numpy', 'scipy', 'matplotlib', 'astropy', 'emcee', 'joblib']
+)
